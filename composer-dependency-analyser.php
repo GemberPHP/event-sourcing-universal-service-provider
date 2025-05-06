8bfb40b0e4fe3089af704f273739063646c0dcb9
Jeroen de Graaf <hello@jero.work>
2025-05-06 19:01:52 +0200

    Add missing dependency symfony/property-access

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 
-return $config;
+return $config
+    ->ignoreErrorsOnPackage('symfony/property-access', [ErrorType::UNUSED_DEPENDENCY]);
